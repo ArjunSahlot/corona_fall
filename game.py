75838a051c62c1b5b7dd4e30b470893ed9192808
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-24 10:22:30 -0800

    licensed /home/arjun/work/programming/tmp/corona_fall/game

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
+#
+#  Corona fall
+#  A pygame project in which Coronavirus falls from the sky and you have to dodge it.
+#  Copyright Arjun Sahlot 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import pygame, os, random
 import tkinter as tk
 
